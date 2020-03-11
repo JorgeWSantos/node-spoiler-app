6ef2f54a9ca2dc76ec6b452f7c52f542071983f7
Jorge <jorgeewillian00@hotmail.com>
2020-03-11 07:15:57 -0300

    criação da abstração da tabela

---
+++ api-node/src/model/spoiler.js
@@ -0,0 +1,36 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../database/database");
+  
+const Spoiler = sequelize.define("spoiler", {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: 1,
+    
+  },
+  titulo: {
+    allowNull: false,
+    type: Sequelize.STRING(255),
+    validate: {
+      len: [2, 255]
+    }
+  },
+  espoliador: {
+    allowNull: false,
+    type: Sequelize.STRING(40),
+    validate: {
+      len: [2, 40]
+    }
+  },
+  descricao: {
+    allowNull: false,
+    type: Sequelize.STRING(255),
+    validate: {
+      len: [2, 255]
+    }
+  }
+});
+  
+   
+  module.exports = Spoiler;
